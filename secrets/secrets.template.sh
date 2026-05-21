65b102957b40632b67d6aa284a74801a53ee4f02
Jesse Abdul <jesse.abdul@noaa.gov>
2026-05-20 19:14:55 -1000

    Updated PRI pointer to add bug fixes to the deploy GIM Updated secrets template

--- secrets/secrets.template.sh
+++ secrets/secrets.template.sh
@@ -1,27 +1,28 @@
 #--- Credentials ---
-# Set your oracle administrator password here: SYS and SYSTEM database schema passwords, APEX administrator password (workspace = INTERNAL, user = ADMIN), and the ORDS_PUBLIC_USER password
-ORACLE_PWD="YOUR_PASSWORD"
 
 # define the default admin user for Oracle
-ORACLE_ADMIN_USER=SYS
+ORACLE_ADMIN_USER="SYS"
+
+# Set your oracle administrator password here: SYS and SYSTEM database schema passwords, APEX administrator password (workspace = INTERNAL, user = ADMIN), and the ORDS_PUBLIC_USER password
+ORACLE_PWD='YOUR_PASSWORD'
 
 # define any database/apex credentials necessary to deploy the database schemas and/or applications
 
 # define DSC credentials
-DSC_USER=DSC
-DSC_PWD="YOUR_DSC_PASSWORD"
+DSC_USER="DSC"
+DSC_PWD='YOUR_DSC_PASSWORD'
 
 # define PRI schema credentials
 DB_PRI_USER="PRI"
-DB_PRI_PASSWORD="YOUR_PRI_PASSWORD"
+DB_PRI_PASSWORD='YOUR_PRI_PASSWORD'
 
 # GIM schema credentials
 DB_GIM_USER="PRI_GIM_APP"
-DB_GIM_PASSWORD="YOUR_GIM_PASSWORD"
+DB_GIM_PASSWORD='YOUR_GIM_PASSWORD'
 
 # RIA schema credentials
 DB_RIA_USER="PRI_RIA_APP"
-DB_RIA_PASSWORD="YOUR_RIA_PASSWORD"
+DB_RIA_PASSWORD='YOUR_RIA_PASSWORD'
 
 # define the git api key for refreshing the database
 GIT_API_KEY='YOUR_API_KEY'
