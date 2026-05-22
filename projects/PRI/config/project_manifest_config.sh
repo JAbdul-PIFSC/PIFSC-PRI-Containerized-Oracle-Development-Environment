073d7f6af87ddcb569ffa37f32ecaf05f213fa3f
Jesse Abdul <jesse.abdul@noaa.gov>
2026-05-21 15:35:29 -1000

    added comments to project_manifest_config.sh Updated Appropriate Usage section to PRI CODE Fork Documentation.md Updated README.md to fix unreadable characters

--- projects/PRI/config/project_manifest_config.sh
+++ projects/PRI/config/project_manifest_config.sh
@@ -2,11 +2,17 @@
 
 	# define the database scripts mapping using the pipe character as a delimiter
 	# The elements should contain encoded values with the "|" character as the delimiter: sql path (within container)|sql script file|User Secret Name|Password Secret Name|Script Password Secrets (this can be one or more optional pipe-delimited secret names when a password is injected into the script - examples include a CREATE USER command) 
+
+	# create schemas script - executed as SYSDBA
 	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/PRI/modules/PRI/shared_SQL|@dev_container_setup/create_docker_schemas.sql|oracle_admin_user|oracle_pwd|pri_db_password_secret|pri_gim_db_password_secret|pri_ria_db_password_secret")
+	
+	# create PRI data schema objects
 	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/PRI/modules/PRI/shared_SQL|@automated_deployments/deploy_dev.sql|pri_db_username_secret|pri_db_password_secret")
 
-
+	# create PRI RIA schema objects
 	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/PRI/modules/PRI/shared_SQL|@automated_deployments/deploy_RIA_dev.sql|pri_ria_db_username_secret|pri_ria_db_password_secret")
+
+	# create PRI GIM schema objects
 	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/PRI/modules/PRI/shared_SQL|@automated_deployments/deploy_GIM_dev.sql|pri_gim_db_username_secret|pri_gim_db_password_secret")
 
 	# define the array of non-sensitive environment variable names that are exported for use in the container
@@ -34,5 +40,3 @@
 		  ["pri_db_password_secret"]="DB_PRI_PASSWORD"
 		  ["pri_git_api_key_secret"]="GIT_API_KEY"
 		)
-	
-	
