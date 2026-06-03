9c08dc2deec537a712713eb8d6a4e69a151449d3
Jesse Abdul <jesse.abdul@noaa.gov>
2026-06-03 07:26:38 -1000

    Moved pri_runtime_config.sh into the config folder to keep only custom scripts in the hooks folder

--- projects/PRI/hooks/pre_client_local_hook.sh
+++ projects/PRI/hooks/pre_client_local_hook.sh
@@ -3,4 +3,4 @@
 echo "This is the PRI pre client_local hook"
 
 # load the PRI runtime configuration file
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/pri_runtime_config.sh"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../config/pri_runtime_config.sh"
