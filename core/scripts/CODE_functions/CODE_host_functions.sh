0747695e9ca634a4110200cb09b3b7f177a5e0e5
Jesse Abdul <jesse.abdul@noaa.gov>
2026-06-03 08:12:08 -1000

    temporary debugging update

--- core/scripts/CODE_functions/CODE_host_functions.sh
+++ core/scripts/CODE_functions/CODE_host_functions.sh
@@ -50,7 +50,7 @@ function code_host_execute_container_scripts()
 		env_block+=$'\n'"$(cds_shared_generate_export_env_vars_block ${CUSTOM_ENV_VARS[@]})"
 	fi
 
-	# echo "DEBUG: The value of the env_block is: ${env_block}"
+	echo "DEBUG: The value of the env_block is: ${env_block}"
 
 	# declare the function arguments as a local variable
 	local -A func_args=(
